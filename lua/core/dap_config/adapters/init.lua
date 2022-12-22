e079cc5faf4683f6a6a1e34898e05aef51c8d72d
Nikita Chursin <chursinn91@gmail.com>
2022-12-22 15:13:38 +0300

    remove runtime_config and neotest mappings

--- lua/core/dap_config/adapters/init.lua
+++ lua/core/dap_config/adapters/init.lua
@@ -1,21 +1,2 @@
 require('core.dap_config.adapters.go')
 require('core.dap_config.adapters.js')
-
-
-
--- require('dap-go').setup {
---   -- Additional dap configurations can be added.
---   -- dap_configurations accepts a list of tables where each entry
---   -- represents a dap configuration. For more details do:
---   -- :help dap-configuration
---   dap_configurations = {
---     {
---       -- Must be "go" or it will be ignored by the plugin
---       type = "go",
---       name = "Attach remote",
---       mode = "remote",
---       request = "attach",
---     },
---   },
--- }
-
--- lua/core/testing/mappings.lua
+++ lua/core/testing/mappings.lua
@@ -1,32 +1,28 @@
-local cmd = vim.cmd
 local mappings_utils = require('utils.mappings_utils')
 local map = mappings_utils.map
 
--- cmd [[
--- function! DebugNearest()
---   let g:test#go#runner = 'delve'
---   TestNearest
---   unlet g:test#go#runner
--- endfunction
--- ]]
+function _G.set_js_runner(runner)
+    vim.g['test#javascript#runner'] = runner
+end
 
--- map('n', 't<C-n>', ':TestNearest<CR>', { noremap = false })
--- map('n', 't<C-f>', ':TestFile<CR>', { noremap = false })
--- map('n', 't<C-l>', ':TestLast<CR>', { noremap = false })
--- map('n', 't<C-d>', ':call DebugNearest<CR>', { noremap = false })
--- map('n', 't<C-s>', ':TestSuite<CR>', { noremap = false })
--- map('n', 't<C-g>', ':TestVisit<CR>', { noremap = false })
-map('n', 't<C-n>', ':lua require("neotest").run.run()<CR>', { noremap = false })
-map('n', 't<C-f>', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', { noremap = false })
-map('n', 't<C-d>', ':lua require("neotest").run.run({ strategy = "dap" })<CR>', { noremap = false })
-map('n', 't<C-l>', ':lua require("neotest").run.run_last()<CR>', { noremap = false })
-map('n', 't<C-s>', ':lua require("neotest").summary.toggle()<CR>', { noremap = false })
-map('n', 't<C-t>', ':lua require("neotest").output_panel.toggle()<CR>', { noremap = false })
+map('n', 't<C-n>', ':TestNearest<CR>', { noremap = false })
+map('n', 't<C-f>', ':TestFile<CR>', { noremap = false })
+map('n', 't<C-l>', ':TestLast<CR>', { noremap = false })
+map('n', 't<C-d>', ':call DebugNearest<CR>', { noremap = false })
+map('n', 't<C-s>', ':TestSuite<CR>', { noremap = false })
+map('n', 't<C-g>', ':TestVisit<CR>', { noremap = false })
 
-map('n', 'tcjw', ":lua require'core.runtime_config'.switch_js_test_runner('webdriverio')<CR>", { noremap = false })
-map('n', 'tcjm', ":lua require'core.runtime_config'.switch_js_test_runner('mocha')<CR>", { noremap = false })
+map('n', 'tcjw', ":lua set_js_runner('webdriverio')<CR>", { noremap = false })
+map('n', 'tcjm', ":lua set_js_runner('mocha')<CR>", { noremap = false })
 -- map('n', 'tcjw', ':let g:test#javascript#runner="webdriverio"<CR>', { noremap = false })
 -- map('n', 'tcjm', ':let g:test#javascript#runner="mocha"<CR>', { noremap = false })
 map('n', 'tcjj', ':let g:test#javascript#runner="jest"<CR>', { noremap = false })
 map('n', 'tsj', ':echo g:test#javascript#runner<CR>', { noremap = false })
 
+
+-- map('n', 't<C-n>', ':lua require("neotest").run.run()<CR>', { noremap = false })
+-- map('n', 't<C-f>', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', { noremap = false })
+-- map('n', 't<C-d>', ':lua require("neotest").run.run({ strategy = "dap" })<CR>', { noremap = false })
+-- map('n', 't<C-l>', ':lua require("neotest").run.run_last()<CR>', { noremap = false })
+-- map('n', 't<C-s>', ':lua require("neotest").summary.toggle()<CR>', { noremap = false })
+-- map('n', 't<C-t>', ':lua require("neotest").output_panel.toggle()<CR>', { noremap = false })
