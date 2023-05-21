44182d4dc7ffa51d2c5c60a55f59d8e78f9c14f2
Nikita Chursin <chursinn91@gmail.com>
2023-05-21 08:42:41 +0300

    added hotkeys for test runners

--- lua/core/testing/mappings.lua
+++ lua/core/testing/mappings.lua
@@ -1,21 +1,31 @@
 local mappings_utils = require('utils.mappings_utils')
 local map = mappings_utils.map
+local cmd = vim.cmd
 
-function _G.set_js_runner(runner)
-    vim.g['test#javascript#runner'] = runner
-end
+-- function _G.set_js_runner(runner)
+--     cmd[[
+--     let test#javascript#runner=
+--     ]]
+--     vim.g['test#javascript#runner'] = runner
+-- end
 
-map('n', 't<C-n>', ':TestNearest<CR>', { noremap = false })
-map('n', 't<C-f>', ':TestFile<CR>', { noremap = false })
-map('n', 't<C-l>', ':TestLast<CR>', { noremap = false })
-map('n', 't<C-d>', ':call DebugNearest<CR>', { noremap = false })
-map('n', 't<C-s>', ':TestSuite<CR>', { noremap = false })
-map('n', 't<C-g>', ':TestVisit<CR>', { noremap = false })
+-- map('n', 't<C-n>', ':TestNearest<CR>', { noremap = false })
+-- map('n', 't<C-f>', ':TestFile<CR>', { noremap = false })
+-- map('n', 't<C-l>', ':TestLast<CR>', { noremap = false })
+-- map('n', 't<C-d>', ':call DebugNearest<CR>', { noremap = false })
+-- map('n', 't<C-s>', ':TestSuite<CR>', { noremap = false })
+-- map('n', 't<C-g>', ':TestVisit<CR>', { noremap = false })
+map('n', 'tn', ':TestNearest<CR>', { noremap = false })
+map('n', 'tf', ':TestFile<CR>', { noremap = false })
+map('n', 'tl', ':TestLast<CR>', { noremap = false })
+map('n', 'td', ':call DebugNearest<CR>', { noremap = false })
+map('n', 'ts', ':TestSuite<CR>', { noremap = false })
+map('n', 'tg', ':TestVisit<CR>', { noremap = false })
 
-map('n', 'tcjw', ":lua set_js_runner('webdriverio')<CR>", { noremap = false })
-map('n', 'tcjm', ":lua set_js_runner('mocha')<CR>", { noremap = false })
--- map('n', 'tcjw', ':let g:test#javascript#runner="webdriverio"<CR>', { noremap = false })
--- map('n', 'tcjm', ':let g:test#javascript#runner="mocha"<CR>', { noremap = false })
+-- map('n', 'tcjw', ":lua set_js_runner('webdriverio')<CR>", { noremap = false })
+-- map('n', 'tcjm', ":lua set_js_runner('mocha')<CR>", { noremap = false })
+map('n', 'tcjw', ':let g:test#javascript#runner="webdriverio"<CR>', { noremap = false })
+map('n', 'tcjm', ':let g:test#javascript#runner="mocha"<CR>', { noremap = false })
 map('n', 'tcjj', ':let g:test#javascript#runner="jest"<CR>', { noremap = false })
 map('n', 'tsj', ':echo g:test#javascript#runner<CR>', { noremap = false })
 
--- lua/core/testing/vim_test.lua
+++ lua/core/testing/vim_test.lua
@@ -1,6 +1,12 @@
+local cmd = vim.cmd
+
+cmd[[
+let test#javascript#mocha#file_pattern = '\v((test|spec)s?/.*|(spec))\.(js|jsx|coffee|ts)$'
+let test#javascript#webdriverio#file_pattern = '\v((test|spec)s?/.*|(test|spec))\.(js|jsx|coffee|ts)$'
+]]
+
+
 vim.g['test#strategy'] = "neovim"
-vim.g['test#javascript#mocha#file_pattern'] = '\v((test|spec)s?/.*|(spec)).(js|jsx|coffee|ts)$'
-vim.g['test#javascript#webdriverio#file_pattern'] = '\v((test|spec)s?/.*|(test)).(js|jsx|coffee|ts)$'
 vim.g['test#javascript#mocha#executable'] = 'mocha'
 -- vim.g['test#javascript#runner'] = 'jest'
 vim.g['test#javascript#runner'] = 'mocha'
