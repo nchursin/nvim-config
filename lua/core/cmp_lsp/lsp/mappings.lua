8b59f3fc41d6afe2718f28792e6cab5e529ef146
Nikita Chursin <nchursin.dev@gmail.com>
2023-06-05 22:18:57 +0300

    use telescope to browse implementation

--- lua/core/cmp_lsp/lsp/mappings.lua
+++ lua/core/cmp_lsp/lsp/mappings.lua
@@ -9,7 +9,6 @@ local bufopts = {
 map('n', 'gd', '<cmd> lua vim.lsp.buf.definition()<CR>', bufopts)
 map('n', 'gsd', '<C-w>v<cmd> lua vim.lsp.buf.definition()<CR>', bufopts)
 map('n', '<leader>gd', '<C-w>v<cmd> lua vim.lsp.buf.definition()<CR>', bufopts)
-map('n', 'gi', '<cmd> lua vim.lsp.buf.implementation()<CR>', bufopts)
 
 map('n', '<C-k>', '<cmd> lua vim.lsp.buf.signature_help()<CR>', bufopts)
 map('i', '<C-k>', '<cmd> lua vim.lsp.buf.signature_help()<CR>', bufopts)
@@ -17,5 +16,6 @@ map('i', '<C-k>', '<cmd> lua vim.lsp.buf.signature_help()<CR>', bufopts)
 map('n', '<leader>rn', '<cmd> lua vim.lsp.buf.rename()<CR>', bufopts)
 map('n', '<leader>qf', '<cmd> lua vim.lsp.buf.code_action()<CR>', bufopts)
 
+map('n', 'gi', '<cmd> lua vim.g.helpers.open_implementations()<CR>', bufopts)
 map('n', 'gr', '<cmd> lua vim.g.helpers.open_references()<CR>', bufopts)
 
--- lua/core/helpers.lua
+++ lua/core/helpers.lua
@@ -5,5 +5,9 @@ vim.g.helpers = {
         -- vim.lsp.buf.references()
         telescope.lsp_references()
     end,
+    open_implementations = function()
+        -- vim.lsp.buf.implementation()
+        telescope.lsp_implementations()
+    end,
 }
 
