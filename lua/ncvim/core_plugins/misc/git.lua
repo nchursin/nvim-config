18452dbddded01eb34c497ddbce52a26e81c1409
Nikita Chursin <nchursin.dev@gmail.com>
2023-06-19 11:00:17 +0300

    fix gitsigns

--- lua/ncvim/core_plugins/misc/git.lua
+++ lua/ncvim/core_plugins/misc/git.lua
@@ -15,7 +15,11 @@ ncvim.plugin({
   },
   {
     'lewis6991/gitsigns.nvim',
-    tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+    -- tag = 'release',
+    config = function()
+      require('gitsigns').setup({
+      })
+    end
   },
 })
 
