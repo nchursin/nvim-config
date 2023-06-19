6b6b7ff7d4f74854076b8287a55b2d3ab633557a
Nikita Chursin <nchursin.dev@gmail.com>
2023-06-19 21:39:58 +0300

    no session auto-load

--- lua/ncvim/ui/welcome_screen/init.lua
+++ lua/ncvim/ui/welcome_screen/init.lua
@@ -48,14 +48,24 @@ ncvim.plugin({
       [[                                  :++++++++.   .+++++***.                                 ]],
       [[                                                                                          ]],
     }
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "󰉋 Go to folder", ":lua ncvim.filetree.focus()<CR>:2q<CR>"),
-      dashboard.button("s", "󰍛 Sessions", ":SessionManager load_session<CR>"),
-      dashboard.button("m", " LSP/Linters/Formatters", ":Mason<CR>"),
-      dashboard.button("p", " Packages Update (PackerSync)", ":PackerSync<CR>"),
-      dashboard.button("q", "󰩈 Quit", ":qa<CR>"),
-    }
+
+    local session_manager_config = require('session_manager.config')
+    local cwd = vim.loop.cwd()
+    local current_dir_session = session_manager_config.dir_to_session_filename(cwd)
+
+    local buttons = {}
+    table.insert(buttons, dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"))
+    table.insert(buttons, dashboard.button("f", "󰉋 Go to folder", ":lua ncvim.filetree.focus()<CR>:2q<CR>"))
+    if current_dir_session:exists() then
+      table.insert(buttons,
+        dashboard.button("l", "󰦖 Load Current Dir Session", ":SessionManager load_current_dir_session<CR>"))
+    end
+    table.insert(buttons, dashboard.button("s", "󱣹 Sessions", ":SessionManager load_session<CR>"))
+    table.insert(buttons, dashboard.button("m", " LSP/Linters/Formatters", ":Mason<CR>"))
+    table.insert(buttons, dashboard.button("p", " Packages Update (PackerSync)", ":PackerSync<CR>"))
+    table.insert(buttons, dashboard.button("q", "󰩈 Quit", ":qa<CR>"))
+
+    dashboard.section.buttons.val = buttons
 
     dashboard.config.opts.noautocmd = true
     alpha.setup(dashboard.config)
