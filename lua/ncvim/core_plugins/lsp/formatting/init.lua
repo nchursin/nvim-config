2adadb2ec0334225b311ffdb9e9dec06f208d3e1
Nikita Chursin <chursinn91@gmail.com>
2024-01-16 15:44:01 +0300

    formatting

---
+++ lua/ncvim/core_plugins/lsp/formatting/init.lua
@@ -0,0 +1,34 @@
+ncvim.plugin({
+    'mhartington/formatter.nvim',
+    config = function()
+        -- local util = require "formatter.util"
+        require("formatter").setup({
+            -- Enable or disable logging
+            logging = true,
+            -- Set the log level
+            log_level = vim.log.levels.WARN,
+            filetype = {
+                javascript = {
+                    require("formatter.filetypes.javascript").prettierd,
+                },
+                typescript = {
+                    require("formatter.filetypes.typescript").prettierd,
+                },
+                typescriptreact = {
+                    require("formatter.filetypes.typescriptreact").prettierd,
+                },
+                javascriptreact = {
+                    require("formatter.filetypes.javascriptreact").prettierd,
+                },
+            },
+        })
+    end
+})
+
+ncvim.add_autocmd(
+    'BufWritePost',
+    {
+        pattern = '*',
+        command = ':FormatWrite',
+    }
+)
