bbde0affd7664eaf038c77d6b35181f1eb061d68
Nikita Chursin <nchursin.dev@gmail.com>
2024-06-07 13:48:40 +0300

    added dir name to statusline

--- lua/ncvim/ui/statusline.lua
+++ lua/ncvim/ui/statusline.lua
@@ -7,6 +7,24 @@ ncvim.plugin({
     }
   },
   config = function()
-    require('lualine').setup({})
+    function basename(str)
+      local name = string.gsub(str, "(.*/)(.*)", "%2")
+      return name
+    end
+
+    function get_cwd_basename()
+      return '󰙅 ' .. basename(vim.fn.getcwd())
+    end
+
+    require('lualine').setup({
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { get_cwd_basename, 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+    })
   end
 })
