e5181551db8be2eedb4f043b53af6bc9d1ae448e
Nikita Chursin <nchursin.dev@gmail.com>
2024-12-15 21:46:22 +0300

    gotestsum

--- lua/ncvim/core_plugins/testing/neotest.lua
+++ lua/ncvim/core_plugins/testing/neotest.lua
@@ -14,11 +14,60 @@ ncvim.plugin({
   --     require("neotest-golang"),     -- Registration
   --   },
   -- }
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-golang"),   -- Registration
+  -- config = function()
+  --   require("neotest").setup({
+  --     adapters = {
+  --       require("neotest-golang")({
+  --         runner = "gotestsum"
+  --       }),
+  --     },
+  --   })
+  -- end,
+
+  opts = function(_, opts)
+    opts.adapters = opts.adapters or {}
+    opts.adapters["neotest-golang"] = {
+      runner = "gotestsum",
+      gotestsum_args = {
+        '--format=testdox',
       },
-    })
+      -- go_test_args = {
+      --   "-v",
+      --   "-race",
+      --   "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+      -- },
+    }
+  end,
+  config = function(_, opts)
+    if opts.adapters then
+      local adapters = {}
+      for name, config in pairs(opts.adapters or {}) do
+        if type(name) == "number" then
+          if type(config) == "string" then
+            config = require(config)
+          end
+          adapters[#adapters + 1] = config
+        elseif config ~= false then
+          local adapter = require(name)
+          if type(config) == "table" and not vim.tbl_isempty(config) then
+            local meta = getmetatable(adapter)
+            if adapter.setup then
+              adapter.setup(config)
+            elseif adapter.adapter then
+              adapter.adapter(config)
+              adapter = adapter.adapter
+            elseif meta and meta.__call then
+              adapter(config)
+            else
+              error("Adapter " .. name .. " does not support setup")
+            end
+          end
+          adapters[#adapters + 1] = adapter
+        end
+      end
+      opts.adapters = adapters
+    end
+
+    require("neotest").setup(opts)
   end,
 })
